2a71f7935f6784413406e96dbd261a926a3b8c37
Lyle Franklin <lfranklin@pivotal.io>
2016-08-03 20:28:11 -0700

    Add manifest to deploy to Cloud Foundry
    - Also copies Staticfile (needed for buildpack) and nginx.conf (enable
      static gzipping)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,19 @@
-var CompressionPlugin = require("compression-webpack-plugin");
+var CompressionPlugin = require('compression-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
 
 exports.modifyWebpackConfig = function(config, env) {
   if (env !== 'develop') {
     config.plugin('compression-plugin', CompressionPlugin, [{
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
       test: /\.js$|\.html$|\.css$/,
       threshold: 2048,
       minRatio: 0
     }])
+    config.plugin('additional-copy-plugin', CopyWebpackPlugin, [[
+      { from: path.join(__dirname, 'additional_assets') }
+    ]])
   }
   return config
 }
