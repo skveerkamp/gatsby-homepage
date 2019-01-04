5625f797e389be332e42ab579a29333107a41311
Lyle Franklin <lylejfranklin@gmail.com>
2019-01-04 14:02:50 -0800

    Add Resume content

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,42 +5,79 @@ import StaggeredList from '../components/staggeredList'
 
 const SecondPage = () => (
   <StaggeredList>
-  <div className="container">
-  <h3>Resume</h3>
-  <div className="row">
-  <div className="col-md-8">
-  <h2 className="margin-bottom-1">Experience</h2>
-  <h4>Staff Software Engineer @ Pivotal Cloud Foundry</h4>
-  <div className="date margin-bottom-1">Jan 15' - Present</div>
-  <h5>Engineering Anchor for Release Engineering Team (2018)</h5>
-  <ul>
-    <li>Deployed Cloud Foundry under different configurations to CI environments 50+ times each week</li>
-    <li>Reduced the number of configuration file lines from 60K+ to 17K without removing functionality</li>
-    <li>Designed and built a self-service system to enable teams across the organization to integrate changes</li>
-  </ul>
-  <h5>Engineer for Cloud Foundry API (CAPI) Team (2017)</h5>
-  <ul>
-    <li>Developed microservices using Go and Ruby on Rails for the Cloud Foundry Control Plane</li>
-    <li>Designed a reusable system for engineers to claim an already deployed environment for manual testing, saving hours each week in wasted developer productivity across the company</li>
-    </ul>
-  <h5>Engineering Anchor for BOSH CPI Team (2016)</h5>
-  <ul>
-  <li>Developed components to deploy Cloud Foundry across AWS, GCP, vSphere, or Azure by abstracting away the infrastructure from the rest of the deployment system</li>
-  <li>Created Google CPI to deploy Cloud Foundry to Google Cloud Platform and worked with Google engineers to transfer ownership of the codebase to Google</li>
-  </ul>
-  <h5>Engineering Anchor for Core Services Team (2015)</h5>
-  <ul>
-  <li>Maintained the managed MySQL service for Cloud Foundry</li>
-  <li>Designed and implemented a streaming backup solution to encrypt and compress backups before uploading to a blobstore</li>
-  </ul>
-  </div>
-  <div className="col-md-4">
-  <h2>Education</h2>
-  <h2>Skills</h2>
-  <h2>Tools</h2>
-  </div>
-  </div>
-  </div>
+    <div className="container">
+      <h3>Resume</h3>
+      <div className="row">
+        <div className="col-md-8">
+          <h2 className="margin-bottom-1">Experience</h2>
+          <h4>Staff Software Engineer @ Pivotal Cloud Foundry</h4>
+          <div className="date margin-bottom-1">Jan '15 - Present</div>
+          <h5>Engineering Anchor for Release Engineering Team (2018)</h5>
+          <ul>
+            <li>Deployed Cloud Foundry under different configurations to CI environments 50+ times each week</li>
+            <li>Reduced the number of configuration file lines from 60K+ to 17K without removing functionality</li>
+            <li>Designed and built a self-service system to enable teams across the organization to integrate changes</li>
+          </ul>
+          <h5>Engineer for Cloud Foundry API (CAPI) Team (2017)</h5>
+          <ul>
+            <li>Developed microservices using Go and Ruby on Rails for the Cloud Foundry Control Plane</li>
+            <li>Designed a reusable system for engineers to claim an already deployed environment for manual testing, saving hours each week in wasted developer productivity across the company</li>
+          </ul>
+          <h5>Engineering Anchor for BOSH CPI Team (2016)</h5>
+          <ul>
+            <li>Developed components to deploy Cloud Foundry across AWS, GCP, vSphere, or Azure by abstracting away the infrastructure from the rest of the deployment system</li>
+            <li>Created Google CPI to deploy Cloud Foundry to Google Cloud Platform and worked with Google engineers to transfer ownership of the codebase to Google</li>
+          </ul>
+          <h5>Engineering Anchor for Core Services Team (2015)</h5>
+          <ul>
+            <li>Maintained the managed MySQL service for Cloud Foundry</li>
+            <li>Designed and implemented a streaming backup solution to encrypt and compress backups before uploading to a blobstore</li>
+          </ul>
+
+          <h4>Extreme Blue Intern @ IBM</h4>
+          <div className="date margin-bottom-1">May '14 - Aug '14</div>
+          <ul>
+            <li>One of only 32 hires out of 1,800 applicants (&lt;2% acceptance rate)</li>
+            <li>Built system to allow non-technical users to deploy solutions to IBM's Bluemix cloud hosting service</li>
+            <li>Utilized Vagrant, Docker, and Cloud Foundry to standardize development and production environments</li>
+            <li>Pitched project to executives and distinguished engineers on a weekly basis</li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <h2>Education</h2>
+          <h4>University of Southern California</h4>
+          <h5>M.S. in Computer Science, spec. in Software Engineering</h5>
+					<div>Graduated: Dec '14</div>
+					<div>GPA: 4.00</div>
+					<br/>
+          <h4>Ball State University</h4>
+          <h5>B.S. in Computer Science</h5>
+					<div>Graduated: Dec '12</div>
+					<div>GPA: 3.95</div>
+
+					<br/>
+          <h2>Skills</h2>
+					<h5>Written production code with:</h5>
+          <div>Go, Ruby, Bash, SQL</div>
+          <h5>Played around with:</h5>
+          <div>Java, JavaScript, CSS/LESS/SASS</div>
+          <h5>Deployed with:</h5>
+          <div>Kubernetes, Cloud Foundry, ConcourseCI, Terraform, Ansible</div>
+          <h5>Deployed to:</h5>
+          <div>Google Cloud, AWS, vSphere, Azure</div>
+          <h5>Developed by:</h5>
+          <div>Test Driven Development, Pair Programming</div>
+
+					<br/>
+          <h2>Projects</h2>
+          <ul>
+            <li>2-hour video explaining TLS protocol: <a target="_blank" href="https://youtu.be/yzz3bcnWf7M">https://youtu.be/yzz3bcnWf7M</a></li>
+            <li>Guide  to  deploying  Kubernetes  on Raspberry Pi: <a target="_blank" href="https://github.com/ljfranklin/k8s-pi">https://github.com/ljfranklin/k8s-pi</a></li>
+            <li>Terraform  resource  for  ConcourseCI: <a target="_blank" href="https://github.com/ljfranklin/terraform-resource">https://github.com/ljfranklin/terraform-resource</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
 
   </StaggeredList>
 )
