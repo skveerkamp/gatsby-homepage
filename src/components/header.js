aee1c1020f3f47308dc040fdda2c0240177c3907
Lyle Franklin <lylejfranklin@gmail.com>
2019-01-04 11:55:16 -0800

    Remove nav hamburger menu

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,11 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
-  <nav className="navbar navbar-expand-md fixed-top">
+  <nav className="navbar navbar-expand fixed-top">
   <a className="navbar-brand" href="/">LYLE FRANKLIN</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
 
-  <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-    <ul className="navbar-nav mr-auto">
+  <div>
+    <ul className="navbar-nav">
       <li className="nav-item">
         <Link className="nav-link" to="resume">Resume</Link>
       </li>
